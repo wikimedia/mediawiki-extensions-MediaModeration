0dd94c385c4c630b7decbe455247aa4bd54e9fbb
Dreamy Jazz <wpgbrown@wikimedia.org>
2023-11-15 21:10:56 +0000

    Add maintenance script to import existing files to scan table
    Why:
    * A maintenance script is needed that allows importing images from the
      image, oldimage, and filearchive tables into the
      mediamoderation_scan table so that images that already existed on a
      wiki can be imported into the table.
    * This script should be able to started at any timestamp so that it can
      import images with a timestamp equal or greater to this, but should
      by default import all images.
    * This script should import the images to the new table in batches to
      avoid overloading the database.
    
    What:
    * Create the maintenance script 'importExistingFilesToScanTable.php'
      that allows importing images from the image, oldimage, and
      filearchive tables.
    * Make this script optionally take 'start-timestamp', 'sleep',
      'table', and 'batch-size' options to control where the import
      starts, how long the script should pause between batches,
      the tables to import images from, and the number of rows
      to read from the DB for a batch.
    * Also add a 'mark-complete' option that if specified forces the
      script to be marked as complete (by adding an entry to the
      updatelog). If not specified, the script is only marked as
      complete if the 'start-timestamp' and 'table' options were left
      as the default.
    * Print the status of the import to the console, along with the
      timestamp currently being used as the start point for the batch
      so that the import can be re-started from this timestamp in case
      of failures or early termination of the script.
    * When getting the rows for a batch, ensure that the timestamp for
      the first and last row in the batch are not the same. If they are
      temporarily increase the batch size so that the start and end
      timestamp are not the same. This is done to avoid an infinite loop
      as the timestamp is the only cross-table compatible way to page
      (i.e. split into batches).
    * When processing a row in the batch, create either a LocalFile or
      ArchivedFile object (depending on the table being processed) from
      the row, so that the the object can be passed to the MediaModeration
      FileProcessor in an accepted format.
    * Only attempt to import an image referenced by a row to the
      mediamoderation_scan table if:
    ** The SHA-1 is not false or empty
    ** The SHA-1 does not already exist in the table on a replica DB
    * Unit test each method in the maintenance script to ensure
      reliability.
    * Integration test the maintenance script to ensure the script
      to also ensure reliability.
    
    Bug: T350863
    Change-Id: I897e5962caf321234287eecf3ebb11463fda880c

---
+++ maintenance/importExistingFilesToScanTable.php
@@ -0,0 +1,431 @@
+<?php
+
+namespace MediaWiki\Extension\MediaModeration\Maintenance;
+
+use ArchivedFile;
+use InvalidArgumentException;
+use LocalFile;
+use LocalRepo;
+use LoggedUpdateMaintenance;
+use MediaWiki\Extension\MediaModeration\Services\MediaModerationDatabaseLookup;
+use MediaWiki\Extension\MediaModeration\Services\MediaModerationFileProcessor;
+use MediaWiki\FileRepo\File\FileSelectQueryBuilder;
+use Wikimedia\Rdbms\IReadableDatabase;
+use Wikimedia\Rdbms\SelectQueryBuilder;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class ImportExistingFilesToScanTable extends LoggedUpdateMaintenance {
+
+	/** @var string[] The DB tables that have images to be imported to mediamoderation_scan. */
+	public const TABLES_TO_IMPORT_FROM = [
+		'image',
+		'filearchive',
+		'oldimage',
+	];
+
+	private IReadableDatabase $dbr;
+	private MediaModerationFileProcessor $mediaModerationFileProcessor;
+	private MediaModerationDatabaseLookup $mediaModerationDatabaseLookup;
+	private LocalRepo $localRepo;
+
+	public function __construct() {
+		parent::__construct();
+		$this->requireExtension( 'MediaModeration' );
+		$this->addDescription( 'Populates the mediamoderation_scan table with existing images from the wiki.' );
+		$this->addOption(
+			'sleep',
+			'Sleep time (in seconds) between every batch. Default: 1',
+			false,
+			true
+		);
+		$this->addOption(
+			'start-timestamp',
+			'The timestamp which to start importing files from. Default is for no timestamp start point ' .
+			'(which means importing all images)',
+			false,
+			true
+		);
+		$this->addOption(
+			'table',
+			'Allows specifying which table(s) files should be imported from. Default is all supported tables.',
+			false,
+			true,
+			false,
+			true
+		);
+		$this->addOption(
+			'mark-complete',
+			'Allows controlling whether this script should be considered completely run for the purposes ' .
+			'of the updatelog. If provided the script will be marked as complete. ' .
+			"Default is to consider the script completely run if the 'table' and 'start-timestamp' options were left " .
+			'as the default and the script does not error out.',
+		);
+	}
+
+	/** @inheritDoc */
+	protected function getUpdateKey() {
+		return __CLASS__;
+	}
+
+	/** @inheritDoc */
+	protected function doDBUpdates() {
+		$loadBalancerFactory = $this->getServiceContainer()->getDBLoadBalancerFactory();
+		$this->dbr = $loadBalancerFactory->getReplicaDatabase( 'virtual-mediamoderation' );
+		$this->localRepo = $this->getServiceContainer()->getRepoGroup()->getLocalRepo();
+		$this->mediaModerationFileProcessor = $this->getServiceContainer()->get( 'MediaModerationFileProcessor' );
+		$this->mediaModerationDatabaseLookup = $this->getServiceContainer()->get( 'MediaModerationDatabaseLookup' );
+
+		// Get the list of tables to import images from.
+		$tablesToProcess = $this->getTablesToProcess();
+		if ( $tablesToProcess === false ) {
+			// If the value is false, then return false
+			// as this indicates the script did not run.
+			return false;
+		}
+
+		foreach ( $tablesToProcess as $table ) {
+			$batchSize = $this->getBatchSize() ?? 200;
+			$this->output( "Now importing rows from the table '$table' in batches of $batchSize.\n" );
+			$previousBatchFinalTimestamp = $this->getOption( 'start-timestamp', '' );
+			if ( $previousBatchFinalTimestamp ) {
+				$this->output(
+					"Starting from timestamp $previousBatchFinalTimestamp and importing files with a " .
+					"greater timestamp.\n"
+				);
+			}
+			$expectedNumberOfBatches = $this->getEstimatedNumberOfBatchesForTable(
+				$table, $previousBatchFinalTimestamp
+			);
+			$batchNo = 1;
+			do {
+				$outputString = "Batch $batchNo of ~$expectedNumberOfBatches";
+				if ( $previousBatchFinalTimestamp ) {
+					$outputString .= " with rows starting at timestamp $previousBatchFinalTimestamp";
+				}
+				$this->output( $outputString . ".\n" );
+				[
+					$filesLeft,
+					$previousBatchFinalTimestamp
+				] = $this->performBatch( $table, $previousBatchFinalTimestamp );
+				sleep( intval( $this->getOption( 'sleep', 1 ) ) );
+				$loadBalancerFactory->waitForReplication();
+				$batchNo += 1;
+			} while ( $filesLeft );
+		}
+		$returnValue = $this->generateDBUpdatesReturnValue();
+		if ( $returnValue ) {
+			$this->output( "Script marked as completed (added to updatelog).\n" );
+		} else {
+			$this->output(
+				'Script not marked as completed (not added to updatelog). The script was marked as not complete ' .
+				"because not all the images on the wiki were processed in this run of the script.\n" .
+				'To mark the script as complete and not have it run again through update.php, make sure to run the ' .
+				"script again with the 'mark-complete' option specified. You should only do this once you are sure " .
+				"that all the images on the wiki have been imported.\n"
+			);
+		}
+		return $returnValue;
+	}
+
+	/**
+	 * Return the value that should be used as the return value
+	 * of ::doDBUpdates. This value depends on the options
+	 * passed to the script.
+	 *
+	 * If true is returned, it should only be done if the script
+	 * either has imported all images or the caller of the maintenance
+	 * script has specifically intended for the script to marked as
+	 * complete.
+	 *
+	 * @return bool
+	 */
+	protected function generateDBUpdatesReturnValue(): bool {
+		// Return true if mark-complete is specified, or if both:
+		// * start-timestamp is not specified or an empty string, and
+		// * table is not specified or includes all tables listed in self::TABLES_TO_IMPORT_FROM.
+		return $this->hasOption( 'mark-complete' ) ||
+			(
+				$this->getOption( 'start-timestamp', '' ) === '' &&
+				$this->getOption( 'table', self::TABLES_TO_IMPORT_FROM ) == self::TABLES_TO_IMPORT_FROM
+			);
+	}
+
+	/**
+	 * Processes the user supplied list of tables to process,
+	 * with the default being all supported tables.
+	 *
+	 * Prints an error if the supplied arguments are invalid.
+	 *
+	 * @return false|array The list of tables to process, or false if the list was not valid.
+	 */
+	protected function getTablesToProcess() {
+		$tablesToProcess = $this->getOption( 'table', self::TABLES_TO_IMPORT_FROM );
+		if ( !count( $tablesToProcess ) ) {
+			$this->error( "The array of tables to have images imported from cannot be empty.\n" );
+			return false;
+		}
+		foreach ( $tablesToProcess as $table ) {
+			if ( !in_array( $table, self::TABLES_TO_IMPORT_FROM ) ) {
+				$this->error( "The table option value '$table' is not a valid table to import images from.\n" );
+				return false;
+			}
+		}
+		return $tablesToProcess;
+	}
+
+	/**
+	 * Gets the expected number of batches needed to process a table.
+	 * This is used just for visual display and the actual number of batches
+	 * may be higher or lower.
+	 *
+	 * @param string $table
+	 * @param string $startTimestamp The timestamp that the processing will start from.
+	 * @return int
+	 */
+	protected function getEstimatedNumberOfBatchesForTable( string $table, string $startTimestamp ): int {
+		// Get the row count for the $table.
+		$queryBuilder = $this->dbr->newSelectQueryBuilder()
+			->select( 'COUNT(*)' )
+			->from( $table );
+		if ( $startTimestamp ) {
+			$queryBuilder->where( $this->dbr->expr(
+				$this->getTimestampFieldForTable( $table ),
+				'>=',
+				$startTimestamp
+			) );
+		}
+		$rowCountInTable = $queryBuilder
+			->caller( __METHOD__ )
+			->fetchField();
+		// If the row count is zero, then one batch will be performed.
+		if ( !$rowCountInTable ) {
+			return 1;
+		}
+		// The expected batch count is the number of rows in the table
+		// divided by the batch size. This may be higher than the actual
+		// batch count, as it may be temporarily increased to prevent
+		// infinite loops.
+		$batchSize = $this->getBatchSize() ?? 200;
+		$expectedBatchesCount = ceil( $rowCountInTable / $batchSize );
+		if ( $rowCountInTable % $batchSize === 0 ) {
+			// If the batch size divides the row count without a remainder, then
+			// the expected batch count needs to be increased by one as one
+			// more batch will be performed at the end with no rows found.
+			$expectedBatchesCount += 1;
+		}
+		return $expectedBatchesCount;
+	}
+
+	/**
+	 * Gets the temporary batch size for use by ::processBatch if it was provided
+	 * with $shouldRaiseBatchSize as the boolean 'true'. This is one more than
+	 * the number of files with the $previousBatchFinalTimestamp.
+	 *
+	 * @param FileSelectQueryBuilder $fileSelectQueryBuilder The cloned FileSelectQueryBuilder that is being
+	 *   built in ::getSelectFileQueryBuilder. This needs to be cloned to avoid issues with this
+	 *   method modifying the query builder.
+	 * @param string $timestampField
+	 * @param string $previousBatchFinalTimestamp
+	 * @return int
+	 */
+	protected function getTemporaryBatchSize(
+		FileSelectQueryBuilder $fileSelectQueryBuilder, string $timestampField, string $previousBatchFinalTimestamp
+	): int {
+		$filesWithTheCutoffTimestamp = (int)$fileSelectQueryBuilder
+			->clearFields()
+			->field( 'COUNT(*)' )
+			->where( $this->dbr->expr( $timestampField, '=', $previousBatchFinalTimestamp ) )
+			->caller( __METHOD__ )
+			->fetchField();
+		// Sanity check that the new batch size would actually be larger (otherwise
+		// leave the batch size as is as it will be fine).
+		if ( $filesWithTheCutoffTimestamp >= ( $this->getBatchSize() ?? 200 ) ) {
+			$batchSize = $filesWithTheCutoffTimestamp + 1;
+			$this->output(
+				"Temporarily raised the batch size to $batchSize due to files with the same upload timestamp. " .
+				"This is done to prevent an infinite loop. Consider raising the batch size to avoid this.\n"
+			);
+			return $batchSize;
+		}
+		return $this->getBatchSize() ?? 200;
+	}
+
+	/**
+	 * Gets the timestamp field for the given $table.
+	 *
+	 * @param string $table One of 'image', 'oldimage', or 'fileimage'
+	 * @return string The timestamp field name
+	 * @throws InvalidArgumentException If $table is not one of the three valid options.
+	 */
+	protected function getTimestampFieldForTable( string $table ): string {
+		if ( $table === 'image' ) {
+			return 'img_timestamp';
+		} elseif ( $table === 'oldimage' ) {
+			return 'oi_timestamp';
+		} elseif ( $table === 'filearchive' ) {
+			return 'fa_timestamp';
+		} else {
+			throw new InvalidArgumentException( "Unrecognised image table '$table'." );
+		}
+	}
+
+	/**
+	 * Gets the appropriate FileSelectQueryBuilder for the $table and
+	 * applies the WHERE conditions, ORDER BY and LIMIT.
+	 *
+	 * @param string $table The table name currently being processed.
+	 * @param string $previousBatchFinalTimestamp The timestamp which the last batch stopped at. This
+	 *   is used to filter for files with this timestamp or a newer timestamp.
+	 * @param bool $shouldRaiseBatchSize Used to indicate that the previous batch ended and started on
+	 *   the same timestamp, so this batch should reattempt that timestamp
+	 *   but with a temporarily raised batch size to account for this.
+	 * @return FileSelectQueryBuilder
+	 */
+	protected function getFileSelectQueryBuilder(
+		string $table, string $previousBatchFinalTimestamp, bool $shouldRaiseBatchSize
+	): FileSelectQueryBuilder {
+		// Get the appropriate FileSelectQueryBuilder using the ::newFor* methods.
+		if ( $table === 'image' ) {
+			$fileSelectQueryBuilder = FileSelectQueryBuilder::newForFile( $this->dbr );
+		} elseif ( $table === 'oldimage' ) {
+			$fileSelectQueryBuilder = FileSelectQueryBuilder::newForOldFile( $this->dbr );
+		} elseif ( $table === 'filearchive' ) {
+			$fileSelectQueryBuilder = FileSelectQueryBuilder::newForArchivedFile( $this->dbr );
+		} else {
+			throw new InvalidArgumentException( "Unrecognised image table '$table'." );
+		}
+		$timestampField = $this->getTimestampFieldForTable( $table );
+		$batchSize = $this->getBatchSize() ?? 200;
+		if ( $shouldRaiseBatchSize ) {
+			// If the previous batch started and ended on the same timestamp,
+			// then temporarily raise the batch count to 1 more than the number
+			// of files with this timestamp to avoid an infinite loop.
+			$batchSize = $this->getTemporaryBatchSize(
+				clone $fileSelectQueryBuilder,
+				$timestampField,
+				$previousBatchFinalTimestamp
+			);
+		}
+		// If the timestamp is not empty, filter for entries with a greater timestamp
+		// than the cutoff timestamp.
+		if ( $previousBatchFinalTimestamp ) {
+			$fileSelectQueryBuilder
+				->where( $this->dbr->expr( $timestampField, '>=', $previousBatchFinalTimestamp ) );
+		}
+		// Order by the timestamp (oldest to newest) and set the limit as the batch size.
+		$fileSelectQueryBuilder
+			->orderBy( $timestampField, SelectQueryBuilder::SORT_ASC )
+			->limit( $batchSize );
+		return $fileSelectQueryBuilder;
+	}
+
+	/**
+	 * Get the LocalFile or ArchiveFile object for the $row.
+	 * The exact object type depends on the $table.
+	 *
+	 * @param \stdClass $row
+	 * @param string $table
+	 * @return ArchivedFile|LocalFile
+	 */
+	protected function getFileObjectForRow( $row, $table ) {
+		if ( $table === 'image' || $table === 'oldimage' ) {
+			return $this->localRepo->newFileFromRow( $row );
+		} elseif ( $table === 'filearchive' ) {
+			return ArchivedFile::newFromRow( $row );
+		} else {
+			throw new InvalidArgumentException( "Unrecognised image table '$table'." );
+		}
+	}
+
+	/**
+	 * Gets the rows for a batch along with the timestamp for the last file in the batch.
+	 *
+	 * @param string $table
+	 * @param string $previousBatchFinalTimestamp
+	 * @return array The rows for the batch, the timestamp for the last file in the results list, and the
+	 *   LIMIT used for the batch.
+	 */
+	protected function getRowsForBatch( string $table, string $previousBatchFinalTimestamp ): array {
+		// Get the FileSelectQueryBuilder with everything but the caller specified.
+		$fileSelectQueryBuilder = $this->getFileSelectQueryBuilder(
+			$table, $previousBatchFinalTimestamp, false
+		);
+		// Specify the caller and then get the rows from the DB.
+		$rows = $fileSelectQueryBuilder
+			->caller( __METHOD__ )
+			->fetchResultSet();
+		$lastFileTimestamp = $previousBatchFinalTimestamp;
+		// Check whether the last file in this batch has the same timestamp as in
+		// $previousBatchFinalTimestamp. If so, then increase the batch size to
+		// prevent an infinite loop which would be caused by processing the same
+		// files over and over again with that timestamp.
+		if ( $rows->numRows() ) {
+			$rows->seek( $rows->numRows() - 1 );
+			$lastFileObject = $this->getFileObjectForRow( $rows->fetchObject(), $table );
+			if ( $previousBatchFinalTimestamp === $lastFileObject->getTimestamp() ) {
+				// Temporarily raise the batch size for the next batch as the
+				// last timestamp in this batch is the same as the last timestamp
+				// for the last batch.
+				$fileSelectQueryBuilder = $this->getFileSelectQueryBuilder(
+					$table, $previousBatchFinalTimestamp, true
+				);
+				$rows = $fileSelectQueryBuilder
+					->caller( __METHOD__ )
+					->fetchResultSet();
+				$rows->seek( $rows->numRows() - 1 );
+				$lastFileObject = $this->getFileObjectForRow( $rows->fetchObject(), $table );
+			}
+			// Store the timestamp for the last file, and return it to the caller later in this method.
+			$lastFileTimestamp = $lastFileObject->getTimestamp();
+			// Reset the position of the pointer for the caller to be able to use a foreach loop on $rows.
+			$rows->rewind();
+		}
+		return [ $rows, $lastFileTimestamp, $fileSelectQueryBuilder->getQueryInfo()['options']['LIMIT'] ];
+	}
+
+	/**
+	 * Perform a batch of imports to the mediamoderation_scan table from the $table
+	 * starting at the $lastFileTimestamp going towards newer files.
+	 *
+	 * @param string $table
+	 * @param string $previousBatchFinalTimestamp
+	 * @return array First value is whether another batch should be run, second value is the new value of
+	 *   $previousBatchFinalTimestamp, and third value is the new value of $shouldRaiseBatchSize
+	 */
+	protected function performBatch( string $table, string $previousBatchFinalTimestamp ): array {
+		[ $rows, $lastFileTimestamp, $batchSizeUsedForBatch ] = $this->getRowsForBatch(
+			$table, $previousBatchFinalTimestamp
+		);
+		foreach ( $rows as $row ) {
+			// Get the File or ArchivedFile object for this $row.
+			$fileObject = $this->getFileObjectForRow( $row, $table );
+			// Exclude any file that has a SHA-1 value that is false or empty.
+			// This can happen in some filearchive rows where the image no
+			// longer exists.
+			// Also check if the SHA-1 exists in the scan table using a replica DB
+			// before attempting to insert the file to reduce the number of
+			// unnecessary reads on the primary DB.
+			if (
+				$fileObject->getSha1() &&
+				!$this->mediaModerationDatabaseLookup->fileExistsInScanTable( $fileObject )
+			) {
+				$this->mediaModerationFileProcessor->insertFile( $fileObject );
+			}
+		}
+		// Return false as the first item of the array if the number of rows processed
+		// was less than the batch size. This will happen when there are no more images
+		// to process.
+		return [
+			$rows->count() >= $batchSizeUsedForBatch,
+			$lastFileTimestamp
+		];
+	}
+}
+
+$maintClass = ImportExistingFilesToScanTable::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ tests/phpunit/integration/maintenance/ImportExistingFilesToScanTableTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace MediaWiki\Extension\MediaModeration\Tests\Integration\Maintenance;
+
+use MediaWiki\Extension\MediaModeration\Maintenance\ImportExistingFilesToScanTable;
+use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use MediaWiki\Tests\Unit\Permissions\MockAuthorityTrait;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @covers \MediaWiki\Extension\MediaModeration\Maintenance\ImportExistingFilesToScanTable
+ * @group MediaModeration
+ * @group Database
+ */
+class ImportExistingFilesToScanTableTest extends MaintenanceBaseTestCase {
+	use MockAuthorityTrait;
+
+	protected function getMaintenanceClass() {
+		return ImportExistingFilesToScanTable::class;
+	}
+
+	public function testExecuteWithNoRowsToImport() {
+		/** @var TestingAccessWrapper $maintenance */
+		$maintenance = $this->maintenance;
+		// Set sleep as 0, otherwise the tests will take ages.
+		$maintenance->setOption( 'sleep', 0 );
+		// Run the maintenance script
+		$maintenance->execute();
+		$this->expectOutputString(
+			"Now importing rows from the table 'image' in batches of 200.\n" .
+			"Batch 1 of ~1.\n" .
+			"Now importing rows from the table 'filearchive' in batches of 200.\n" .
+			"Batch 1 of ~1.\n" .
+			"Now importing rows from the table 'oldimage' in batches of 200.\n" .
+			"Batch 1 of ~1.\n" .
+			"Script marked as completed (added to updatelog).\n"
+		);
+		// Expect no rows in mediamoderation_scan, as the import should have added nothing.
+		$this->assertSame(
+			0,
+			(int)$this->getDb()->newSelectQueryBuilder()
+				->select( 'COUNT(*)' )
+				->table( 'mediamoderation_scan' )
+				->caller( __METHOD__ )
+				->fetchField(),
+			'The importExistingFilesToScanTable.php maintenance script added rows to mediamoderation_scan ' .
+			'when no rows existed in the image, oldimage, or filearchive tables.'
+		);
+		// Should have a row in the updatelog
+		$this->assertSame(
+			1,
+			(int)$this->getDb()->newSelectQueryBuilder()
+				->select( 'COUNT(*)' )
+				->table( 'updatelog' )
+				->caller( __METHOD__ )
+				->fetchField(),
+			'The updatelog table should have a entry as the script completed.'
+		);
+	}
+
+	public function testExecuteWithInvalidTableProvided() {
+		/** @var TestingAccessWrapper $maintenance */
+		$maintenance = $this->maintenance;
+		// Set sleep as 0, otherwise the tests will take ages.
+		$maintenance->setOption( 'sleep', 0 );
+		// Set the 'table' option to include an unsupported table.
+		$maintenance->setOption( 'table', [ 'image', 'invalidtable', 'oldimage' ] );
+		// Run the maintenance script
+		$maintenance->execute();
+		$this->expectOutputString(
+			"The table option value 'invalidtable' is not a valid table to import images from.\n"
+		);
+		// Expect no rows in mediamoderation_scan, as the import should have added nothing.
+		$this->assertSame(
+			0,
+			(int)$this->getDb()->newSelectQueryBuilder()
+				->select( 'COUNT(*)' )
+				->table( 'mediamoderation_scan' )
+				->caller( __METHOD__ )
+				->fetchField(),
+			'The importExistingFilesToScanTable.php maintenance script added rows to mediamoderation_scan ' .
+			'when no rows existed in the image, oldimage, or filearchive tables.'
+		);
+		// Should have no row in the updatelog
+		$this->assertSame(
+			0,
+			(int)$this->getDb()->newSelectQueryBuilder()
+				->select( 'COUNT(*)' )
+				->table( 'updatelog' )
+				->caller( __METHOD__ )
+				->fetchField(),
+			'The updatelog table should be empty as the script was not run.'
+		);
+	}
+
+	public function testExecuteWithMarkCompleteSpecified() {
+		// Set sleep as 0, otherwise the tests will take ages.
+		$this->maintenance->setOption( 'sleep', 0 );
+		// Define that mark-complete is provided.
+		$this->maintenance->setOption( 'mark-complete', 1 );
+		// Run the maintenance script
+		$this->maintenance->execute();
+		$this->expectOutputString(
+			"Now importing rows from the table 'image' in batches of 200.\n" .
+			"Batch 1 of ~1.\n" .
+			"Now importing rows from the table 'filearchive' in batches of 200.\n" .
+			"Batch 1 of ~1.\n" .
+			"Now importing rows from the table 'oldimage' in batches of 200.\n" .
+			"Batch 1 of ~1.\n" .
+			"Script marked as completed (added to updatelog).\n"
+		);
+		// Expect no rows in mediamoderation_scan, as the import should have added nothing.
+		$this->assertSame(
+			0,
+			(int)$this->getDb()->newSelectQueryBuilder()
+				->select( 'COUNT(*)' )
+				->table( 'mediamoderation_scan' )
+				->caller( __METHOD__ )
+				->fetchField(),
+			'The importExistingFilesToScanTable.php maintenance script added rows to mediamoderation_scan ' .
+			'when no rows existed in the image, oldimage, or filearchive tables.'
+		);
+		// Should have a row in the updatelog
+		$this->assertSame(
+			1,
+			(int)$this->getDb()->newSelectQueryBuilder()
+				->select( 'COUNT(*)' )
+				->table( 'updatelog' )
+				->caller( __METHOD__ )
+				->fetchField(),
+			'The updatelog table should have a entry as mark-complete was provided.'
+		);
+	}
+}
