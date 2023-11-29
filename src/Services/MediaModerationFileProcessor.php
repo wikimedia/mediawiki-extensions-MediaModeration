6919445309011bb67f40e4e75869bc3b629f55cc
Dreamy Jazz <wpgbrown@wikimedia.org>
2023-11-29 00:05:52 +0000

    Only allow drawing and bitmap media types to be scanned
    Why:
    * Files that are not drawings or bitmaps cannot be properly
      represented by an image. As such, even if they could be
      transformed to an accepted image (as determined by
      File::canRender) attempting to scan the image would not be
      useful.
    * As such, File or ArchivedFile objects that have their
      ::getMediaType method return anything other than MEDIATYPE_BITMAP
      or MEDIATYPE_DRAWING should have MediaModerationFileProcessor
      ::canScanFile return false.
    
    What:
    * Add the private constant ALLOWED_MEDIA_TYPES to the
      MediaModerationFileProcessor service that stores the values
      MEDIATYPE_BITMAP and MEDIATYPE_DRAWING.
    * In MediaModerationFileProcessor::canScanFile, return false
      always if the return value of the File::getMediaType or
      ArchivedFile::getMediaType is not in the ALLOWED_MEDIA_TYPES
      constant.
    * Split the ::canScanFile method into several private helper
      methods. This is to improve readability by reducing the
      amount of nesting.
    * Update tests for this new code.
    
    Bug: T352234
    Change-Id: Ibe2df9b1824699401a942cef2943d747bf812c3b
    (cherry picked from commit 8edd14d382ec47064b31d18dd1f232190c7c0909)

--- src/Services/MediaModerationFileProcessor.php
+++ src/Services/MediaModerationFileProcessor.php
@@ -17,6 +17,12 @@ class MediaModerationFileProcessor {
 		'image/tiff',
 	];
 
+	/** @var array An array of mediatypes that can be properly converted to an accepted mime type for PhotoDNA. */
+	private const ALLOWED_MEDIA_TYPES = [
+		MEDIATYPE_BITMAP,
+		MEDIATYPE_DRAWING,
+	];
+
 	private MediaModerationDatabaseManager $mediaModerationDatabaseManager;
 	private MediaHandlerFactory $mediaHandlerFactory;
 	private LoggerInterface $logger;
@@ -29,6 +35,72 @@ class MediaModerationFileProcessor {
 		$this->mediaModerationDatabaseManager = $mediaModerationDatabaseManager;
 		$this->mediaHandlerFactory = $mediaHandlerFactory;
 		$this->logger = $logger;
+	}
+
+	/**
+	 * Returns whether a file has an allowed media type.
+	 * This check is needed because some files may be
+	 * renderable but not in a supported format (T352234).
+	 *
+	 * @param File|ArchivedFile $file
+	 * @return bool
+	 */
+	private function fileHasAllowedMediaType( $file ): bool {
+		return in_array( $file->getMediaType(), self::ALLOWED_MEDIA_TYPES, true );
+	}
+
+	/**
+	 * Returns whether a file has an allowed mime type
+	 * and therefore could be sent directly to PhotoDNA
+	 * without having to convert the file type.
+	 *
+	 * @param File|ArchivedFile $file
+	 * @return bool
+	 */
+	private function fileHasAllowedMimeType( $file ): bool {
+		return in_array( $file->getMimeType(), self::ALLOWED_MIME_TYPES, true );
+	}
+
+	/**
+	 * Returns whether a file can be likely rendered,
+	 * which is the result of File::canRender. The behaviour
+	 * is similar for ArchivedFile objects.
+	 *
+	 * @param File|ArchivedFile $file
+	 * @return bool
+	 */
+	private function fileCanBeRendered( $file ): bool {
+		if ( $file instanceof File ) {
+			return $file->canRender();
+		}
+		$mediaHandler = $this->mediaHandlerFactory->getHandler( $file->getMimeType() );
+		if ( $mediaHandler ) {
+			try {
+				// This suppression and passing of ArchivedFile to a MediaHandler method
+				// which expects a File object is in the same way as ArchivedFile::pageCount.
+				// TODO: Fix me if ArchivedFile ever extends File.
+				// @phan-suppress-next-line PhanTypeMismatchArgument
+				$fileCanBeRendered = $mediaHandler->canRender( $file );
+			} catch ( Error $e ) {
+				// All errors need to be caught, because a method not existing
+				// will raise the generic in-built Error exception.
+				// If the MediaHandler raises an exception for any reason the
+				// result of this method will be false, and no further actions
+				// would be taken for this file.
+				$this->logger->error(
+					'Call to MediaHandler::canRender with an ArchivedFile did not work ' .
+					'for handler {handlerclass}',
+					[
+						'handlerclass' => get_class( $mediaHandler ),
+						'exception' => $e
+					]
+				);
+				return false;
+			}
+			// The ArchivedFile::exists check is done to make this similar to File::canRender.
+			return $fileCanBeRendered && $file->exists();
+		}
+		return false;
 	}
 
 	/**
@@ -46,39 +118,11 @@ class MediaModerationFileProcessor {
 	 * @return bool
 	 */
 	public function canScanFile( $file ): bool {
-		$canScanFile = in_array( $file->getMimeType(), self::ALLOWED_MIME_TYPES, true );
-		if ( $file instanceof File ) {
-			$canScanFile = $canScanFile || $file->canRender();
-		} elseif ( !$canScanFile ) {
-			// Only attempt to check if the file can be rendered if the above
-			// check failed as the ArchivedFile canRender check is complicated.
-			$mediaHandler = $this->mediaHandlerFactory->getHandler( $file->getMimeType() );
-			if ( $mediaHandler ) {
-				try {
-					// This suppression and passing of ArchivedFile to a MediaHandler method
-					// which expects a File object is in the same way as ArchivedFile::pageCount.
-					// TODO: Fix me if ArchivedFile ever extends File.
-					// @phan-suppress-next-line PhanTypeMismatchArgument
-					$fileCanBeRendered = $mediaHandler->canRender( $file );
-				} catch ( Error $e ) {
-					// All errors need to be caught, because a method not existing
-					// will raise the generic in-built Error exception.
-					// If the MediaHandler raises an exception for any reason the
-					// result of this method will be false, and no further actions
-					// would be taken for this file.
-					$this->logger->error(
-						'Call to MediaHandler::canRender with an ArchivedFile did not work for handler {handlerclass}',
-						[
-							'handlerclass' => get_class( $mediaHandler ),
-							'exception' => $e
-						]
-					);
-					$fileCanBeRendered = false;
-				}
-				// The ArchivedFile::exists check is done to make this similar to File::canRender.
-				$canScanFile = $fileCanBeRendered && $file->exists();
-			}
-		}
+		$canScanFile = $this->fileHasAllowedMediaType( $file ) &&
+			(
+				$this->fileHasAllowedMimeType( $file ) ||
+				$this->fileCanBeRendered( $file )
+			);
 		if ( !$canScanFile ) {
 			$this->logger->debug(
 				'File with SHA-1 {sha1} cannot be scanned by PhotoDNA',
