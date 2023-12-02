403e6e3feb10db0ec85c843096a5ebb752995b5a
Dreamy Jazz <wpgbrown@wikimedia.org>
2023-12-02 00:31:15 +0000

    Update MediaModerationDatabaseLookup
    Why:
    * The MediaModerationDatabaseLookup service needs to be extended to
      return SHA-1 values from rows that meet certian criteria.
    
    What:
    * Add MediaModerationDatabaseLookup::getSha1ValuesForScan that returns
      SHA-1 values as an array which match the given criteria for last
      checked and match status.
    * Add MediaModerationDatabaseLookup::newSelectQueryBuilder that returns
      a SelectQueryBuilder instance that is set up for reading rows from
      the mediamoderation_scan table.
    * Add ::getDateFromTimestamp that given a timestamp in a format that
      can be read by ConvertibleTimestamp, converts it to into a string
      containing just the date in the form YYYYMMDD.
    * Add a private method ::getDb that de-duplicates getting a database
      connection in the service.
    * Add and update phpunit tests.
    
    Bug: T351409
    Change-Id: I4d29daa63d777581bd811794605b32a4b1ff4a4b

---
+++ tests/phpunit/integration/Services/MediaModerationDatabaseLookupTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace MediaWiki\Extension\MediaModeration\Tests\Integration\Services;
+
+use MediaWiki\Extension\MediaModeration\Services\MediaModerationDatabaseLookup;
+use MediaWiki\Tests\Unit\Permissions\MockAuthorityTrait;
+use MediaWikiIntegrationTestCase;
+use Wikimedia\Rdbms\SelectQueryBuilder;
+
+/**
+ * @covers \MediaWiki\Extension\MediaModeration\Services\MediaModerationDatabaseLookup
+ * @group MediaModeration
+ * @group Database
+ */
+class MediaModerationDatabaseLookupTest extends MediaWikiIntegrationTestCase {
+	use MockAuthorityTrait;
+
+	/** @dataProvider provideGetRowsFromScanTable */
+	public function testGetRowsFromScanTable(
+		$limit, $lastChecked, $direction, $matchStatus, $expectedResults, $shouldCompareInOrder
+	) {
+		/** @var MediaModerationDatabaseLookup $objectUnderTest */
+		$objectUnderTest = $this->getServiceContainer()->get( 'MediaModerationDatabaseLookup' );
+		$returnedResults = $objectUnderTest->getSha1ValuesForScan(
+			$limit, $lastChecked, $direction, $matchStatus
+		);
+		$this->assertArrayEquals(
+			$expectedResults,
+			$returnedResults,
+			$shouldCompareInOrder,
+			$shouldCompareInOrder,
+			'::getRowsFromScanTable did not return the expected rows.'
+		);
+	}
+
+	public static function provideGetRowsFromScanTable() {
+		return [
+			'Limit 100, last checked as (fake) current date, any match status, sort ASC' => [
+				// Limit parameter for ::getRowsFromScanTable
+				100,
+				// Last checked parameter for ::getRowsFromScanTable
+				'20231211143402',
+				// The $direction parameter for ::getRowsFromScanTable
+				SelectQueryBuilder::SORT_ASC,
+				// The $matchStatus parameter for ::getRowsFromScanTable
+				MediaModerationDatabaseLookup::ANY_MATCH_STATUS,
+				// The SHA-1 values that should be returned
+				[
+					'sy02psim0bgdh0jt4vdltuzoh7j80yu',
+					'sy02psim0bgdh0jt4vdltuzoh7j80ru',
+					'sy02psim0bgdh0jt4vdltuzoh7j70ru',
+					'sy02psim0bgdh0jt4vdltuzoh7j800u',
+					'sy02psim0bgdh0st4vdltuzoh7j70ru',
+					'sy02psim0bgdh0st4vdltuzoh7j60ru',
+					'sy02psim0bgdh0st4vdlguzoh7j60ru',
+				],
+				// Whether the expected results above should be compared with or without respect to order.
+				false,
+			],
+			'Limit 5, last checked as null, match status as null, sort DESC' => [
+				5,
+				null,
+				SelectQueryBuilder::SORT_DESC,
+				MediaModerationDatabaseLookup::NULL_MATCH_STATUS,
+				[
+					'sy02psim0bgdh0jt4vdltuzoh7j80yu',
+					'sy02psim0bgdh0jt4vdltuzoh7j80ru',
+					'sy02psim0bgdh0jt4vdltuzoh7j70ru',
+					'sy02psim0bgdh0jt4vdltuzoh7j800u'
+				],
+				false,
+			],
+			'Limit 2, last checked as (fake) yesterday, match status any, sort DESC' => [
+				2,
+				'20231210143402',
+				SelectQueryBuilder::SORT_DESC,
+				MediaModerationDatabaseLookup::ANY_MATCH_STATUS,
+				[
+					'sy02psim0bgdh0st4vdlguzoh7j60ru',
+					'sy02psim0bgdh0st4vdltuzoh7j60ru',
+				],
+				true,
+			],
+			'Limit 3, last checked as (fake) current date, match status positive, sort DESC' => [
+				3,
+				'20231211143402',
+				SelectQueryBuilder::SORT_DESC,
+				MediaModerationDatabaseLookup::POSITIVE_MATCH_STATUS,
+				[ 'sy02psim0bgdh0st4vdltuzoh7j60ru' ],
+				true,
+			],
+			'Limit 10, last checked as (fake) current date, match status negative, sort DESC' => [
+				3,
+				'20231211143402',
+				SelectQueryBuilder::SORT_DESC,
+				MediaModerationDatabaseLookup::NEGATIVE_MATCH_STATUS,
+				[
+					'sy02psim0bgdh0st4vdltuzoh7j70ru',
+					'sy02psim0bgdh0st4vdlguzoh7j60ru',
+				],
+				true,
+			],
+		];
+	}
+
+	public function addDBData() {
+		parent::addDBData();
+		$this->db->newInsertQueryBuilder()
+			->insertInto( 'mediamoderation_scan' )
+			->rows( [
+				[ 'mms_sha1' => 'sy02psim0bgdh0jt4vdltuzoh7j80yu' ],
+				[ 'mms_sha1' => 'sy02psim0bgdh0jt4vdltuzoh7j80ru' ],
+				[ 'mms_sha1' => 'sy02psim0bgdh0jt4vdltuzoh7j70ru' ],
+				[ 'mms_sha1' => 'sy02psim0bgdh0jt4vdltuzoh7j800u' ]
+			] )
+			->execute();
+		$this->db->newInsertQueryBuilder()
+			->insertInto( 'mediamoderation_scan' )
+			->rows( [
+				[
+					'mms_sha1' => 'sy02psim0bgdh0st4vdltuzoh7j70ru',
+					// Define last checked as today (mock this as 11/12/2023)
+					'mms_last_checked' => '20231211',
+					'mms_is_match' => 0,
+				],
+				[
+					'mms_sha1' => 'sy02psim0bgdh0st4vdltuzoh7j60ru',
+					// Set last checked as 7 days ago from 11/12/2023.
+					'mms_last_checked' => '20231204',
+					'mms_is_match' => 1,
+				],
+				[
+					'mms_sha1' => 'sy02psim0bgdh0st4vdlguzoh7j60ru',
+					// Set last checked as 1 day ago from 11/12/2023.
+					'mms_last_checked' => '20231210',
+					'mms_is_match' => 0,
+				],
+			] )
+			->execute();
+	}
+}
--- tests/phpunit/unit/Services/MediaModerationDatabaseLookupTest.php
+++ tests/phpunit/unit/Services/MediaModerationDatabaseLookupTest.php
@@ -6,16 +6,22 @@ use ArchivedFile;
 use File;
 use IDBAccessObject;
 use MediaWiki\Extension\MediaModeration\Services\MediaModerationDatabaseLookup;
+use MediaWiki\Tests\Unit\MockServiceDependenciesTrait;
 use MediaWikiUnitTestCase;
 use Wikimedia\Rdbms\IConnectionProvider;
 use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\IReadableDatabase;
 use Wikimedia\Rdbms\SelectQueryBuilder;
+use Wikimedia\Timestamp\ConvertibleTimestamp;
+use Wikimedia\Timestamp\TimestampException;
 
 /**
  * @covers \MediaWiki\Extension\MediaModeration\Services\MediaModerationDatabaseLookup
  * @group MediaModeration
  */
 class MediaModerationDatabaseLookupTest extends MediaWikiUnitTestCase {
+	use MockServiceDependenciesTrait;
+
 	/** @dataProvider provideFileExistsInScanTable */
 	public function testFileExistsInScanTable( $flags, $methodName, $fileObjectClass ) {
 		$mockDb = $this->createMock( IDatabase::class );
@@ -80,6 +86,188 @@ class MediaModerationDatabaseLookupTest extends MediaWikiUnitTestCase {
 			],
 			'Accepts an ArchivedFile object when reading from primary' => [
 				IDBAccessObject::READ_LATEST, 'getPrimaryDatabase', ArchivedFile::class
+			],
+		];
+	}
+
+	/** @dataProvider provideValidTimestamps */
+	public function testGetDateFromTimestamp( $timestamp, $expectedReturnValue ) {
+		/** @var MediaModerationDatabaseLookup $objectUnderTest */
+		$objectUnderTest = $this->newServiceInstance( MediaModerationDatabaseLookup::class, [] );
+		$this->assertSame(
+			$expectedReturnValue,
+			$objectUnderTest->getDateFromTimestamp( $timestamp ),
+			'::getDateFromTimestamp did not return the expected date in the format YYYYMMDD.'
+		);
+	}
+
+	public static function provideValidTimestamps() {
+		return [
+			'ConvertibleTimestamp instance created using an integer' => [
+				new ConvertibleTimestamp( 1234 ),
+				'19700101',
+			],
+			'ConvertibleTimestamp instance created using a string' => [
+				new ConvertibleTimestamp( '20230504030201' ),
+				'20230504',
+			],
+			'String in TS_MW format' => [ '20230405020301', '20230405' ],
+			'Integer (TS_UNIX)' => [ 123456, '19700102' ],
+			'Integer (TS_UNIX) from 2023' => [ 1701725115, '20231204' ],
+		];
+	}
+
+	/** @dataProvider provideInvalidTimestamps */
+	public function testGetDateFromTimestampOnInvalidTimestamp( $timestamp ) {
+		// A TimestampException will be thrown by ConvertibleTimestamp on an invalid timestamp
+		$this->expectException( TimestampException::class );
+		// Get the object under test
+		/** @var MediaModerationDatabaseLookup $objectUnderTest */
+		$objectUnderTest = $this->newServiceInstance( MediaModerationDatabaseLookup::class, [] );
+		$objectUnderTest->getDateFromTimestamp( $timestamp );
+	}
+
+	public static function provideInvalidTimestamps() {
+		return [
+			'String in invalid format' => [ 'abc10245' ],
+		];
+	}
+
+	public function testGetSha1ValuesForScan() {
+		// Create a mock SelectQueryBuilder that expects that
+		// the mms_sha1 field is added, a LIMIT is added and then
+		// the ::fetchFieldValues method is called.
+		$mockDbr = $this->createMock( IReadableDatabase::class );
+		$selectQueryBuilderMock = $this->getMockBuilder( SelectQueryBuilder::class )
+			->setConstructorArgs( [ $mockDbr ] )
+			->onlyMethods( [ 'fetchFieldValues' ] )
+			->getMock();
+		// Make ::fetchFieldValues return some fake SHA-1 values.
+		$selectQueryBuilderMock->expects( $this->once() )
+			->method( 'fetchFieldValues' )
+			->willReturn( [ 'test', 'testing' ] );
+		// Get the object under test, with ::newSelectQueryBuilder mocked
+		$objectUnderTest = $this->getMockBuilder( MediaModerationDatabaseLookup::class )
+			->disableOriginalConstructor()
+			->onlyMethods( [ 'newSelectQueryBuilder' ] )
+			->getMock();
+		$objectUnderTest->expects( $this->once() )
+			->method( 'newSelectQueryBuilder' )
+			->willReturn( $selectQueryBuilderMock );
+		// Call the method under test
+		$this->assertArrayEquals(
+			[ 'test', 'testing' ],
+			$objectUnderTest->getSha1ValuesForScan( 123, '', SelectQueryBuilder::SORT_ASC, '' ),
+			true,
+			true,
+			'::getSha1ValuesForScan did not return the expected results array.'
+		);
+		$this->assertArrayEquals(
+			[ 'mms_sha1' ],
+			$selectQueryBuilderMock->getQueryInfo()['fields'],
+			true,
+			false,
+			'The field being used in the select query was not the SHA-1 field.'
+		);
+		$this->assertSame(
+			123,
+			$selectQueryBuilderMock->getQueryInfo()['options']['LIMIT'],
+			'The wrong LIMIT was used to get the SHA-1 values.'
+		);
+	}
+
+	/** @dataProvider provideNewSelectQueryBuilder */
+	public function testNewSelectQueryBuilder(
+		$lastChecked, $direction, $matchStatus, $dbType, $expectedConds, $expectedOptions
+	) {
+		// Create a mock IConnectionProvider that returns a mock IReadableDatabase
+		$mockConnectionProvider = $this->createMock( IConnectionProvider::class );
+		$mockDbr = $this->createMock( IReadableDatabase::class );
+		$mockConnectionProvider->method( 'getReplicaDatabase' )
+			->willReturn( $mockDbr );
+		// Mock that the $mockDbr returns $dbType as the result of IReadableDatabase::getType
+		$mockDbr->method( 'getType' )
+			->willReturn( $dbType );
+		// Make $mockDbr::newSelectQueryBuilder perform exactly the same thing
+		// as in a real database, but instead using the $mockDbr.
+		$mockDbr->method( 'newSelectQueryBuilder' )
+			->willReturn( new SelectQueryBuilder( $mockDbr ) );
+		// Get the object under test and call the method under test
+		/** @var MediaModerationDatabaseLookup $objectUnderTest */
+		$objectUnderTest = $this->newServiceInstance( MediaModerationDatabaseLookup::class, [
+			'connectionProvider' => $mockConnectionProvider
+		] );
+		$returnedSelectQueryBuilder = $objectUnderTest->newSelectQueryBuilder( $lastChecked, $direction, $matchStatus );
+		// Check that the WHERE conditions and options array returned by ::getQueryInfo
+		// method of the result of ::newSelectQueryBuilder is as expected.
+		$this->assertCount(
+			0,
+			$returnedSelectQueryBuilder->getQueryInfo()['fields'],
+			'No fields should have been added to the query builder returned by ::newSelectQueryBuilder'
+		);
+		$this->assertArrayEquals(
+			[ 'mediamoderation_scan' ],
+			$returnedSelectQueryBuilder->getQueryInfo()['tables'],
+			true,
+			true,
+			'Only the mediamoderation_scan table should have been added to the query builder returned by ' .
+			'::newSelectQueryBuilder.'
+		);
+		$this->assertArrayEquals(
+			$expectedConds,
+			$returnedSelectQueryBuilder->getQueryInfo()['conds'],
+			true,
+			true,
+			'::getQueryInfo for the SelectQueryBuilder returned by ::newSelectQueryBuilder did not return ' .
+			'the expected WHERE conditions, suggesting the query builder was constructed wrong.'
+		);
+		$this->assertArrayEquals(
+			$expectedOptions,
+			$returnedSelectQueryBuilder->getQueryInfo()['options'],
+			'::getQueryInfo for the SelectQueryBuilder returned by ::newSelectQueryBuilder did not return ' .
+			'the expected value, suggesting that the query builder was constructed wrong.'
+		);
+	}
+
+	public static function provideNewSelectQueryBuilder() {
+		return [
+			'Last checked null, direction ASC, any match status, on mariadb' => [
+				// $lastChecked parameter
+				null,
+				// $direction parameter
+				SelectQueryBuilder::SORT_ASC,
+				// $matchStatus parameter
+				MediaModerationDatabaseLookup::ANY_MATCH_STATUS,
+				// String to be returned by IReadableDatabase::getType
+				'mariadb',
+				// The expected WHERE conditions
+				[ 'mms_last_checked' => null ],
+				// The expected 'options' array in the SelectQueryBuilder
+				[ 'ORDER BY' => [ 'mms_last_checked ASC' ] ],
+			],
+			'Last checked as null, direction DESC, match status null, on mariadb' => [
+				null,
+				SelectQueryBuilder::SORT_DESC,
+				null,
+				'mariadb',
+				[ 'mms_last_checked' => null, 'mms_is_match' => null ],
+				[ 'ORDER BY' => [ 'mms_last_checked DESC' ] ],
+			],
+			'Last checked as null, direction DESC, match status null, on postgres' => [
+				null,
+				SelectQueryBuilder::SORT_DESC,
+				'1',
+				'postgres',
+				[ 'mms_last_checked' => null, 'mms_is_match' => '1' ],
+				[ 'ORDER BY' => [ 'mms_last_checked DESC NULLS LAST' ] ],
+			],
+			'Last checked as null, direction ASC, match status as 0, on postgres' => [
+				null,
+				SelectQueryBuilder::SORT_ASC,
+				'0',
+				'postgres',
+				[ 'mms_last_checked' => null, 'mms_is_match' => '0' ],
+				[ 'ORDER BY' => [ 'mms_last_checked ASC NULLS FIRST' ] ],
 			],
 		];
 	}
