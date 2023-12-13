29ea46653a5ccc81db84d50ae8584f7a926dae43
Kosta Harlan <kharlan@wikimedia.org>
2023-12-13 21:42:14 +0100

    maintenance: Add debugPhotoDNA script for verifying file
    Why:
    
    - It's useful to be able to see how the service provider
      implementations (real and mock) operate with a file. Especially for
      the production service provider, it is helpful to see that thumbnail
      transformation works, compare the responses with what we expect, etc.
    
    What:
    
    - Add a basic maintenance script that accepts a file name (the title in
      MediaWiki, without the File: prefix) and uses a mock or real service
      provider to check it
    
    For the mock endpoint, here's example config in LocalSettings.php:
    
    ```
    $wgMediaModerationPhotoDNAMockServiceFiles = [
    	'FilesToIsMatchMap'	=> [
    	    // change to false if you want IsMatch to be false
    		'489px-Lagoon_Nebula.jpg' => true
    	],
    	'FilesToStatusCodeMap' => [
    	    // change to 3000 if you want the status code to be "OK"
    		'489px-Lagoon_Nebula.jpg' => 3206
    	]
    ];
    ```
    
    Bug: T351401
    Change-Id: I9feb955cc39178d1806d122f302b7af4ea3da4b8

---
+++ maintenance/debugPhotoDNA.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MediaWiki\Extension\MediaModeration\Maintenance;
+
+use FormatJson;
+use Maintenance;
+use MediaWiki\Extension\MediaModeration\PhotoDNA\Response;
+use MediaWiki\Extension\MediaModeration\Services\MediaModerationPhotoDNAServiceProvider;
+use MediaWiki\Title\Title;
+use RequestContext;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+/**
+ * Helper script to check how the result from transmitting a local file
+ * via the service provider implementation to PhotoDNA. Does not have
+ * any side effects in the mediamoderation_scan table.
+ */
+class DebugPhotoDNA extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->requireExtension( 'MediaModeration' );
+
+		$this->addDescription( 'Debug sending a single file to the PhotoDNA service endpoint via MediaModeration.' );
+		$this->addArg( 'filename', 'Filename (without File: prefix) to use in check.' );
+		$this->addOption( 'use-mock-endpoint', 'Use the mock API endpoint. Default is false, and to use production.' );
+		$this->addOption( 'raw-json', 'Return only the raw JSON from the endpoint, useful if you want to pipe to jq' );
+	}
+
+	public function execute() {
+		$services = $this->getServiceContainer();
+		$useMock = $this->getOption( 'use-mock-endpoint', false );
+		$photoDNAServiceProvider = $useMock ?
+				$services->get( '_MediaModerationMockPhotoDNAServiceProvider' ) :
+				$services->get( 'MediaModerationPhotoDNAServiceProvider' );
+		if ( !$useMock && !( $photoDNAServiceProvider instanceof MediaModerationPhotoDNAServiceProvider ) ) {
+			$this->fatalError(
+				'Unable to get production service provider.
+				 Check that $wgMediaModerationPhotoDNAUrl and $wgMediaModerationPhotoDNASubscriptionKey are set.'
+			);
+		}
+		$file = $services->getRepoGroup()->getLocalRepo()->findFile(
+			Title::newFromText( $this->getArg(), NS_FILE )
+		);
+		if ( !$file ) {
+			$this->fatalError( 'Unable to get file for ' . $this->getArg() );
+		}
+		$result = $photoDNAServiceProvider->check( $file );
+		/** @var Response|null $response */
+		$response = $result->getValue();
+		$statusFormatter = $services->getFormatterFactory()->getStatusFormatter( RequestContext::getMain() );
+
+		if ( $this->getOption( 'raw-json' ) ) {
+			if ( !$response ) {
+				$this->fatalError( FormatJson::encode( [ 'error' => $statusFormatter->getWikiText( $result ) ] ) );
+			}
+			$this->output( $response->getRawResponse() );
+			return;
+		}
+
+		$this->output( 'StatusValue: ' . ( $result->isGood() ? 'good' : 'bad' ) . PHP_EOL );
+		if ( $response ) {
+			$this->output( 'Status code: ' . $response->getStatusCode() . PHP_EOL );
+		}
+		if ( $result->isGood() && $response ) {
+			$this->output( 'IsMatch: ' . (int)$response->isMatch() . PHP_EOL );
+		} else {
+			$this->output(
+				'Error: ' . $statusFormatter->getWikiText( $result ) . PHP_EOL
+			);
+		}
+		if ( $response ) {
+			$this->output( 'Raw response: ' . PHP_EOL . $response->getRawResponse() . PHP_EOL );
+		}
+	}
+}
+
+$maintClass = DebugPhotoDNA::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
